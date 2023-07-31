0d12cf8b6df72c1e3b098fb31b77ca6366adc632
Adolfo Jimenez <adolfoj@protonmail.com>
2023-07-31 14:27:29 -0400

    Pattern: Model Mixins
    Problem: Distinct models have the same fields and/or methods
    duplicated violating the DRY principle.
    
    Solution: Extract common fields and methods into various
    reusable model mixins.

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class TimeStampedModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now =True)
+
+    class Meta:
+        abstract = True
+
+
+class Postable(TimeStampedModel):
+    message = models.TextField(max_length=500)
+
+    class Meta:
+        abstract = True
+
+
+class Post(Postable):
+    pass
+
+
+class Comment(Postable):
+    pass
+
