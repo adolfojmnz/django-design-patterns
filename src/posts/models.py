3c877c048c2544dcc9ab364333309469e16a17a2
Adolfo Jimenez <adolfoj@protonmail.com>
2023-07-31 16:40:00 -0400

    Pattern: Custom Model Managers
    Problem: Certain queries on models are defined and accessed
    repeatedly throughout the code violating the DRY principle.
    
    Solution: Define custom managers to give meaningful names to
    common queries.

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.conf import settings
+
+from .managers import PostManager
+
 
 class TimeStampedModel(models.Model):
     created = models.DateTimeField(auto_now_add=True)
@@ -16,7 +20,19 @@ class Postable(TimeStampedModel):
 
 
 class Post(Postable):
-    pass
+    POST_PRIVACY = (
+        ('public', 'Public'),
+        ('individual', 'Individual')
+    )
+    privacy = models.CharField(max_length=12, choices=POST_PRIVACY,
+                               default='public')
+    # A recipient is needed if the privacy is set to "Individual"
+    recipient = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                  on_delete=models.CASCADE,
+                                  blank=True,
+                                  null=True,
+                                  related_name="recieved_posts")
+    objects = PostManager()
 
 
 class Comment(Postable):
