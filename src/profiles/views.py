4419b4544edecf8bf7c8560b3946d59361cb3c37
Adolfo Jimenez <adolfoj@protonmail.com>
2023-08-04 23:15:54 -0400

    Pattern: Template Inheritance Tree
    Problem: Templates need lots of common markup in several pages.
    
    Solution: Use template inheritance wherever possible and include
    snippets elsewhere.

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+
+class ProfileListView(TemplateView):
+    template_name = "profiles/index.html"
+
